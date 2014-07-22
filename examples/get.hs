a2fa195ca2011ab4c30f6160d786719ce189e49b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-07-22 17:08:35 +0900

    #45 make simple get client example

---
+++ examples/get.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE PackageImports #-}
+
+import "monads-tf" Control.Monad.Trans
+import Data.Pipe
+import System.Environment
+import Network
+import Network.TigHTTP.Client
+import Network.TigHTTP.Types
+
+import qualified Data.ByteString as BS
+
+main :: IO ()
+main = do
+	addr : pth : _ <- getArgs
+	sv <- connectTo addr $ PortNumber 80
+	p <- request sv $ get addr 80 pth
+	_ <- runPipe $ responseBody p =$= finally printP (putStrLn "")
+	return ()
+
+printP :: MonadIO m => Pipe BS.ByteString () m ()
+printP = await >>= maybe (return ()) (\s -> liftIO (BS.putStr s) >> printP)
