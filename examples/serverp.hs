982385b99220d2ac95ce220fe2796ce14a77a25e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-07-22 17:58:23 +0900

    #45 add POST server example

---
+++ examples/serverp.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE PackageImports #-}
+
+import Control.Monad
+import "monads-tf" Control.Monad.Trans
+import Control.Concurrent
+import Data.Pipe
+import System.Environment
+import Network
+import Network.TigHTTP.Server
+
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as LBS
+
+main :: IO ()
+main = do
+	as <- getArgs
+	soc <- listenOn $ PortNumber 80
+	forever $ do
+		(h, _, _) <- accept soc
+		void . forkIO $ do
+			r <- getRequest h
+			print $ requestPath r
+			void . runPipe $
+				requestBody r =$= printP `finally` putStrLn ""
+			putResponse h . response . LBS.fromChunks $ map BSC.pack as
+
+printP :: MonadIO m => Pipe BSC.ByteString () m ()
+printP = await >>= maybe (return ()) (\s -> liftIO (BSC.putStr s) >> printP)
